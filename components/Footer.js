611aa532d4295d228c3ea67e3149b45439136f72
gritary <driftary@outlook.com>
2025-08-28 13:14:31 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 import { useConfig } from '@/lib/config'
-import Vercel from '@/components/Vercel'
+
 const Footer = ({ fullWidth }) => {
   const BLOG = useConfig()
-
   const d = new Date()
   const y = d.getFullYear()
   const from = +BLOG.since
+
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
@@ -14,11 +14,17 @@ const Footer = ({ fullWidth }) => {
     >
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
-        <div className="flex align-baseline justify-between flex-wrap">
+        <div className="flex flex-col sm:flex-row sm:items-baseline sm:justify-between gap-2">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            © {BLOG.author}{' '}
+            {from === y || !from ? y : `${from} - ${y}`}
           </p>
-          <a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.netlify.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="sm:ml-auto"
+          >
             Hosted on Netlify
           </a>
         </div>
